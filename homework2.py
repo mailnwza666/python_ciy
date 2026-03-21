30d10d0a451a6d026c5997c4a3f1716ee06a0333
mailnwza666 <tonmai.kanomkai@gmail.com>
2026-03-21 09:32:41 +0700

    Create homework2.py
    งานpythonเฉยๆ

---
+++ homework2.py
@@ -0,0 +1,8 @@
+from datetime import date
+
+age = int(input("อายุเท่าไร"))
+#today = date.today()
+#current_year = today.year
+#birthyear = current_year-age
+birthyear = 2026-age
+print("age=", birthyear)
