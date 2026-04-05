db86cf59c70b0371b5859103ac490a8ba81ee0ce
mailnwza666 <tonmai.kanomkai@gmail.com>
2026-04-05 14:16:08 +0700

    Create homework5.py
    งานpythonเฉยๆ

---
+++ homework5.py
@@ -0,0 +1,39 @@
+# price = int(input("ราคาตั๋วต่อใบ"))
+# จำนวน = int(input("จำนวน"))
+# age = int(input("อายุคุณเท่าไร"))
+# ราคารวม = price*จำนวน
+# if age <= 10:
+#     print("ลด50%")
+# elif age >=60:
+#     print("ลด30%")
+# print(ราคารวม)
+
+# number1 = int(input("รับค่าตัวเลข"))
+# number2 = int(input("รับค่าตัวเลข"))
+# number3 = int(input("รับค่าตัวเลข"))
+# number = number1+number2+number3
+# print(number)
+# if number > 100:
+#     print("pass")
+# elif number <= 100:
+#     print("fail")
+
+# number1 = int(input("รับค่าตัวเลข"))
+# number2 = int(input("รับค่าตัวเลข"))
+# number3 = int(input("รับค่าตัวเลข"))
+# if number1 > number2:
+#     if number1 > number3:
+#         print("number1")
+
+
+# elif number2 > number1:
+#       if number2 > number3:
+#           print("number2")
+# elif number3 > number1:
+#       if number3 > number2:
+#           print("number3")
+# else:
+#     print("คำสั่งไม่ถูกต้อง")
+      
+        
+    
