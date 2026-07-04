e7c919e69374cf4d2a05154e5364fa9eaf7006aa
mailnwza666 <tonmai.kanomkai@gmail.com>
2026-07-04 10:30:26 +0700

    Create function and list.py
    งานเฉยๆ

---
+++ function and list.py
@@ -0,0 +1,33 @@
+# scores = []
+
+# def calculate(scores):
+#     total_score = sum(scores)
+#     average = total_score/len(scores)
+#     return total_score, average
+# for x in range(5):
+#     score = int(input("enter your score"))
+#     scores.append(score)
+
+# total_score,average = calculate(scores)
+# print('score',scores)
+# print('total score', total_score)
+# print('average', average)
+
+# hit = 5
+# def bright(hit):
+#     print("อยากเตะเด็ก")
+#     print(hit,'ครั้ง')
+
+# bright(hit)
+
+# numone = 5
+# numtwo = 25
+# numthree = 37
+# def cal(numone,numtwo,numthree):
+#     total_score = numone+numtwo+numthree
+#     avg = total_score/3
+#     return total_score, avg
+
+# total_score, avg = cal(numone,numtwo,numone)
+# print('total score', total_score)
+# print('average', avg)
